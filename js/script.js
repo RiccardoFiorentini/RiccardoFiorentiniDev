d64fb0e6e0ddb0039afc79b6b2bfddcd2cfc35bd
RiccardoFiorentini <rccrd2001@gmail.com>
2023-07-01 19:49:45 +0200

    first version

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+    document.getElementById('my-work-section').scrollIntoView({behavior: "auto"})
+  })
+}
+if(document.getElementById('my-work-experiences-link')){
+  document.getElementById('my-work-experiences-link').addEventListener('click', () => {
+    document.getElementById('my-work-experiences-section').scrollIntoView({behavior: "auto"})
   })
 }
