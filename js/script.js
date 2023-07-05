187952863ec20f82c1f3c08f00d38f9b70e07dc9
RiccardoFiorentini <rccrd2001@gmail.com>
2023-07-05 15:21:54 +0200

    CNR updated and fixed contact me bug

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "auto"})
+    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
 if(document.getElementById('my-work-experiences-link')){
   document.getElementById('my-work-experiences-link').addEventListener('click', () => {
-    document.getElementById('my-work-experiences-section').scrollIntoView({behavior: "auto"})
+    document.getElementById('my-work-experiences-section').scrollIntoView({behavior: "smooth"})
   })
 }
